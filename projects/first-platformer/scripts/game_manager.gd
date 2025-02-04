40c5f47032eb0820322a120dc71cf8e646131207
NPException <npexception42@gmail.com>
2025-02-04 20:05:44 +0100

    add mute music button

--- projects/first-platformer/scripts/game_manager.gd
+++ projects/first-platformer/scripts/game_manager.gd
@@ -9,6 +9,11 @@ extends Node
 var score: int = 0
 
 
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("mute_music"):
+		Music.stream_paused = !Music.stream_paused
+
+
 func add_point() -> void:
 	score += 1
 	score_label.text = "You collected " + str(score) + "/" + str(max_coins) + " coins."
